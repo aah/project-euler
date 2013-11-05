d915690241b679f920a046054a21e1c8eae336cf
Shane Watson <shane@watson.io>
2013-11-05 14:43:01 -0800

    Improve first Python solution

--- python/e001.py
+++ python/e001.py
@@ -8,42 +8,63 @@ If we list all the natural numbers below 10 that are multiples of 3 or
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
-from itertools import count, takewhile
+from fractions import gcd
+from functools import reduce
+from itertools import chain, count, takewhile
 
 
 def sum_multiples_below(limit, divisors):
-    """Sums multiples of one or more divisors below a given limit.
-    >>> sum_multiples_below(10, [2, 3]) == sum([2, 3, 4, 6, 8, 9])
+    """Sum multiples of one or more divisors below a given limit.
+    >>> sum_multiples_below(10, [3, 5]) == 23
     True
     >>> sum_multiples_below(100, [1]) == sum(range(100))
     True
     """
-    return sum(bounded(multiples(divisors), limit))
+    return (sum(multiples_below(limit, divisors)) -
+            sum(repeated_values(limit, divisors)))
 
 
-def multiples(divisors):
-    """Returns a generator that emits the multiples of one or more divisors.
+def multiples_below(limit, divisors):
+    """Make an iterator that returns all natural multiples of each
+    divisor, in order, below some limit (with duplication).
+    >>> list(multiples_below(16, [3, 5]))
+    [3, 6, 9, 12, 15, 5, 10, 15]
+    """
+    return chain.from_iterable(bounded(multiples(d), limit) for d in divisors)
+
+
+def multiples(divisor):
+    """Make an iterator that returns the natural multiples of a divisor.
     >>> from itertools import islice
-    >>> [i for i in islice(multiples([3, 5]), 5)]
-    [0, 3, 5, 6, 9]
+    >>> [i for i in islice(multiples(3), 5)]
+    [3, 6, 9, 12, 15]
     """
-    return (i for i in count() if is_multiple(i, divisors))
+    return count(divisor, divisor)
 
 
-def is_multiple(n, divisors):
-    """Returns True if n is a multiple of *any* number in a list of divisors.
-    >>> is_multiple(9, [2, 3]) and not is_multiple(10, [3, 4])
-    True
+def repeated_values(limit, divisors):
+    """Make an iterator that returns all common, natural multiples of a
+    list of divisors.
+    >>> list(repeated_values(50, [4, 6]))
+    [12, 24, 36, 48]
     """
-    return any(not n % d for d in divisors)
+    return [] if len(divisors) == 1 else multiples_below(limit, [lcm(divisors)])
 
 
 def bounded(g, n):
-    """Returns an iterator containing the terms of g up to n.
+    """Make an iterator containing the terms of g up to n.
     >>> [i for i in bounded(count(), 5)]
     [0, 1, 2, 3, 4]
     """
     return takewhile(lambda x: x < n, g)
+
+
+def lcm(divisors):
+    """Return the least common multiple of a list of divisors.
+    >>> lcm([3, 5])
+    15
+    """
+    return reduce(lambda a, b: a * b // gcd(a, b), divisors)
 
 
 def main():
